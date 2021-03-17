34a0848d35b22c4db412254ac4834635788d6f20
Matthew <cameron.matt@outlook.com>
2021-03-17 23:12:09 +1100

    fetch lab done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+   .then(resp => resp.json())
+   .then(name => renderBooks(name));
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +18,3 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
